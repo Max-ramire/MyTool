9bc1715c3ae55b5a97e4c9a1ff69908b586b1026
DiegoSsj3 <diego.martinez1795@alumnos.udg.mx>
2024-11-25 13:56:38 -0600

    p16 mail

--- sql/mytools.sql
+++ sql/mytools.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 05-09-2024 a las 16:01:07
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Servidor: localhost
+-- Tiempo de generación: 25-11-2024 a las 19:26:06
+-- Versión del servidor: 5.7.44-log
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(50) NOT NULL,
-  `correo` varchar(50) NOT NULL,
-  `clave` varchar(200) NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `correo` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `clave` varchar(200) COLLATE utf8mb4_spanish_ci NOT NULL,
   `fechareg` datetime NOT NULL,
-  `perfil` char(1) NOT NULL
+  `perfil` char(1) COLLATE utf8mb4_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 --
